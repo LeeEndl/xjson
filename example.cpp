ae648d6ffd8718f3e217949cc959723f774c7076
LeeEndl <127639272+LeeEndl@users.noreply.github.com>
2023-04-21 20:48:46 -0500

    Update example.cpp

--- example.cpp
+++ example.cpp
@@ -8,9 +8,17 @@ int main() {
 				{ "another_key", "another_value" }
 			}
 		}/*another group here*/}, "group.txt");
+	// group.txt ->  group_name { { key, value } { another_key, value } }
+	
+	/* reading groups */
+	for (auto& it : xjson::group().Int<int>("group_name", "test.txt")) 
+		if (it.first == "key") it.second; // -> it.first = key; it.second = value
 
+	
 	xjson().write(tmap<std::string>{"key", "value"}, "basic.txt");
-
-	for (auto& it : xjson::group().Int<int>("key", "test.txt")) it.second; // -> the value;
+	// basic.txt -> { key, value }
+	
+	/* reading basic key n value (s) */
+	for (auto& it : xjson().Int<int>("key", "test.txt")) it.second; // -> it.first = key; it.second = value
 	return 0;
 }
